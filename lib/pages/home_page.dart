586fc58002d2d0bab1f1e55aeba13ca8930eaba3
m1toi <147705211+m1toi@users.noreply.github.com>
2024-12-27 17:31:08 +0200

    create ui elements

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:workout_app/data/workout_data.dart';
+import 'workout_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final newWorkoutNameController = TextEditingController();
+
+  void createNewWorkout() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text("Create New Workout"),
+              content: TextField(
+                controller: newWorkoutNameController,
+              ),
+              actions: [
+                MaterialButton(
+                  onPressed: save,
+                  child: Text("Save"),
+                ),
+                MaterialButton(
+                  onPressed: cancel,
+                  child: const Text("Cancel"),
+                )
+              ],
+            ));
+  }
+
+  void goToWorkoutPage(String workoutName) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => WorkoutPage(workoutName: workoutName)));
+  }
+
+  void save() {
+    String workoutName = newWorkoutNameController.text;
+    Provider.of<WorkoutData>(context, listen: false).addWorkout(workoutName);
+
+    Navigator.pop(context);
+    clear();
+  }
+
+  void cancel() {
+    Navigator.pop(context);
+    clear();
+  }
+
+  void clear() {
+    newWorkoutNameController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Consumer<WorkoutData>(
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Workout Tracker"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNewWorkout,
+          child: const Icon(Icons.add),
+        ),
+        body: ListView.builder(
+            itemCount: value.getWorkoutList().length,
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(value.getWorkoutList()[index].name),
+                  trailing: IconButton(
+                    icon: Icon(Icons.arrow_forward_ios),
+                    onPressed: () =>
+                        goToWorkoutPage(value.getWorkoutList()[index].name),
+                  ),
+                )),
+      ),
+    );
   }
 }
